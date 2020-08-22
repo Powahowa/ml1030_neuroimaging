e2ae1ad68564e3408dcb411eead0858c657e9f52
Tony Lee <22460852+ssbmaster@users.noreply.github.com>
2020-08-22 00:15:58 -0400

    nearly complete setup of dataframe for the dataset

--- Tony/feature-extraction.py
+++ Tony/feature-extraction.py
@@ -7,7 +7,10 @@ import pathlib
 import functools
 import time
 from pycaret.classification import *
+import re
 
+# %%
+# Function to find all the regressor file paths
 def timer(func):
     """Print the runtime of the decorated function"""
     @functools.wraps(func)
@@ -22,6 +25,7 @@ def timer(func):
     return wrapper
 
 # %%
+# Function to find all the BOLD NII file paths
 @timer
 def find_paths(relDataFolder, subj, sess, func, patt):
     paths = list(pathlib.Path(relDataFolder).glob(
@@ -32,6 +36,7 @@ def find_paths(relDataFolder, subj, sess, func, patt):
     return paths
 
 # %%
+# Find all the regressor file paths
 regressor_paths = find_paths(relDataFolder='../data/preprocessed',
                             subj='sub-*',
                             sess='ses-*',
@@ -40,6 +45,7 @@ regressor_paths = find_paths(relDataFolder='../data/preprocessed',
 regressor_paths
 
 # %%
+# Find all the BOLD NII file paths
 nii_paths = find_paths(relDataFolder='../data/preprocessed',
                         subj='sub-*',
                         sess='ses-*',
@@ -48,17 +54,81 @@ nii_paths = find_paths(relDataFolder='../data/preprocessed',
 nii_paths
 
 # %%
-session_info = pd.read_csv(
+# Prep for next cell
+session_info_df = pd.read_csv(
         '../data/SleepDiaryData_160320_pseudonymized_final.tsv',
         sep='\t'
     )
+session_info_df
 
 # %%
-df_list = []
-for paths in regressor_paths:
-    temp = pd.DataFrame(pd.read_csv(paths, sep='\t'))
-    df_list.append(temp)
-df = pd.concat(df_list, join='inner', ignore_index=True)
+# Get a mapping Dataframe of subject and which session is the sleep deprived one
+@timer
+def map_sleepdep(session_info):
+    df = pd.DataFrame(session_info.loc[:,['participant_id', 'Sl_cond']])
+    df = df.groupby(['participant_id']).max()
+    return df.rename(columns={'participant_id':'subject', 'Sl_cond':'sleepdep_session'})
+
+sleepdep_map = map_sleepdep(session_info_df)
+sleepdep_map
 
 # %%
-setup(data=df,  target='Legendary')
+# Get Dataframe of subject, session, task, path
+def get_bids_components(paths):
+    components_list = []
+    for path in paths:
+        filename = path.stem
+        matches = re.search(
+            '[a-z0-9]+\-([a-z0-9]+)_[a-z0-9]+\-([a-z0-9]+)_[a-z0-9]+\-([a-z0-9]+)', 
+            filename
+        )
+        subject = matches.group(1)
+        session = matches.group(2)
+        task = matches.group(3)
+        components_list.append([subject, session, task, path])
+    df = pd.DataFrame(components_list, 
+                        columns=['subject', 'session', 'task', 'path']
+                     )
+    return df
+
+components_df = get_bids_components(nii_paths)
+components_df
+
+# %%
+# TODO: Combine logically sleepdep_map and components_df into 1 dataframe
+
+
+# %%
+# Regressors only to be used to further clean up the signal
+# Will need to run GLM through the images or something
+@timer
+def gen_common_regressors(regressor_paths):
+    df_list = []
+    for paths in regressor_paths:
+        temp = pd.DataFrame(pd.read_csv(paths, sep='\t'))
+        df_list.append(temp)
+    return pd.concat(df_list, join='inner', ignore_index=True)
+
+common_regressors = gen_common_regressors(regressor_paths)
+common_regressors
+
+# %%
+# NOTE:
+# !!!!!!!!!!!!!!! ONLY USE BELOW WHEN EVERYTHING IS READY !!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!! ONLY USE BELOW WHEN EVERYTHING IS READY !!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!! ONLY USE BELOW WHEN EVERYTHING IS READY !!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!! ONLY USE BELOW WHEN EVERYTHING IS READY !!!!!!!!!!!!!!!!!!!!!!!
+# !!!!!!!!!!!!!!! ONLY USE BELOW WHEN EVERYTHING IS READY !!!!!!!!!!!!!!!!!!!!!!!
+
+# %%
+# PyCaret setup
+clf1 = setup(data=df,  target='Sleep_Deprived')
+
+# %%
+# PyCaret compare models, uncomment 'compare_models()' to run
+# NOTE: but DON'T DO IT IF LONG RUNTIME!
+# compare_models()
+
+# %%
+# PyCaret create and run SVM
+svm = create_model('svm')
