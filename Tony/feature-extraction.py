9ec8fac220d57cec148298d530e02686db6f9c16
Tony Lee <22460852+ssbmaster@users.noreply.github.com>
2020-08-30 00:00:21 -0400

    Breaking down voxel array data to columns to prep pycaret

--- Tony/feature-extraction.py
+++ Tony/feature-extraction.py
@@ -1,4 +1,6 @@
-# %%
+# %% [markdown]
+# # Metadata Organization
+# ## Imports
 import pandas as pd 
 import numpy as np
 import os.path
@@ -6,11 +8,10 @@ import glob
 import pathlib
 import functools
 import time
-#from pycaret.classification import *
 import re
 
-# %%
-# Function to find all the regressor file paths
+# %% [markdown]
+# ## Function to find all the regressor file paths
 def timer(func):
     """Print the runtime of the decorated function"""
     @functools.wraps(func)
@@ -24,8 +25,8 @@ def timer(func):
         return value
     return wrapper
 
-# %%
-# Function to find all the BOLD NII file paths
+# %% [markdown]
+# ## Function to find all the BOLD NII file paths
 @timer
 def find_paths(relDataFolder, subj, sess, func, patt):
     paths = list(pathlib.Path(relDataFolder).glob(
@@ -35,8 +36,8 @@ def find_paths(relDataFolder, subj, sess, func, patt):
                         
     return paths
 
-# %%
-# Find all the regressor file paths
+# %% [markdown]
+# ## Find all the regressor file paths
 regressor_paths = find_paths(relDataFolder='../data/preprocessed',
                             subj='sub-*',
                             sess='ses-*',
@@ -44,8 +45,8 @@ regressor_paths = find_paths(relDataFolder='../data/preprocessed',
                             patt="*confounds_regressors.tsv")
 regressor_paths
 
-# %%
-# Find all the BOLD NII file paths
+# %% [markdown]
+# ## Find all the BOLD NII file paths
 nii_paths = find_paths(relDataFolder='../data/preprocessed',
                         subj='sub-*',
                         sess='ses-*',
@@ -53,16 +54,16 @@ nii_paths = find_paths(relDataFolder='../data/preprocessed',
                         patt="*MNI152NLin2009cAsym_desc-preproc_bold.nii.gz")
 nii_paths
 
-# %%
-# Prep for next cell
+# %% [markdown]
+# ## Read the participants.tsv file to find summaries of the subjects
 participant_info_df = pd.read_csv(
         '../data/participants.tsv',
         sep='\t'
     )
 participant_info_df
 
-# %%
-# Get a mapping Dataframe of subject and which session is the sleep deprived one
+# %% [markdown]
+# ## Get a mapping Dataframe of subject and which session is the sleep deprived one
 @timer
 def map_sleepdep(participant_info):
     df = pd.DataFrame(participant_info.loc[:,['participant_id', 'Sl_cond']])
@@ -72,8 +73,8 @@ def map_sleepdep(participant_info):
 sleepdep_map = map_sleepdep(participant_info_df)
 sleepdep_map
 
-# %%
-# Get Dataframe of subject, session, task, path
+# %% [markdown]
+# ## Get Dataframe of subject, session, task, path
 def get_bids_components(paths):
     components_list = []
     for path in paths:
@@ -94,33 +95,45 @@ def get_bids_components(paths):
 components_df = get_bids_components(nii_paths)
 components_df
 
-# %%
-# TODO: Combine logically sleepdep_map and components_df into 1 dataframe
+# %% [markdown]
+# ## Combine logically sleepdep_map and components_df into 1 dataframe
 final_df = components_df.merge(sleepdep_map, how='left')
 
-# %%
-# Response column 'sleepdep' imputed from 'session' 'sleepdep_session'
-for i in range(len(final_df)-1):
+# %% [markdown]
+# ## Response column 'sleepdep' imputed from 'session' 'sleepdep_session'
+for i in range(len(final_df)):
     if int(final_df['session'].iloc[i]) == int(final_df['sleepdep_session'].iloc[i]):
         final_df['sleepdep'].iloc[i] = 1
+final_df
 
-# %%
+# %% [markdown]
 from nilearn.input_data import NiftiMasker
 masker = NiftiMasker(mask_img='../masking/sub-9001-9072_resamp_intersected_mask.nii.gz', standardize=False)
 
-#%% 
-fmri_masked = []
+# %% [markdown]
 
-#%%
+
+# %% [markdown]
 final_df["masked array"] = ""
 
-#%%
-for i in range(len(final_df)):
-    f_masked = masker.fit_transform(final_df['path'].iloc[i].__str__())
-    #fmri_masked.append(f_masked)
-    final_df['masked array'].iloc[i] = f_masked
+# %% [markdown]
+def gen_voxel_df(filepath):
+    f_masked = masker.fit_transform(filepath)
+    fmri_masked = pd.DataFrame(np.reshape(f_masked.ravel(), newshape=[1,-1]))
+    print(fmri_masked.shape)
+    return fmri_masked
 
 # %%
+tmp_list = []
+for i in range(len(final_df)):
+    tmp_list.append(gen_voxel_df(final_df['path'].iloc[i]))
+
+# %%
+def merge_df(list):
+    return pd.concat(list, ignore_index=True)
+tmp_df = merge_df(tmp_list)
+
+# %% [markdown]
 # Regressors only to be used to further clean up the signal
 # Will need to run GLM through the images or something
 @timer
@@ -143,8 +156,13 @@ common_regressors
 # !!!!!!!!!!!!!!! ONLY USE BELOW WHEN EVERYTHING IS READY !!!!!!!!!!!!!!!!!!!!!!!
 
 # %%
+tmp_df['sleepdep'] = final_df['sleepdep']
+
+# %%
 # PyCaret setup
-clf1 = setup(data=df,  target='Sleep_Deprived')
+from pycaret.classification import *
+from pycaret.clustering import *
+clf1 = setup(data=tmp_df,  target='sleepdep')
 
 # %%
 # PyCaret compare models, uncomment 'compare_models()' to run
